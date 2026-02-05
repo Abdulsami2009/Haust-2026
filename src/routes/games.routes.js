dd5c138411b9dbe31f1f19cc767e73ee00969029
AbdulSami <abdulsami0108@gmail.com>
2026-02-05 09:54:57 +0000

    update

---
+++ src/routes/games.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const gamesControlller = require('../controllers/games.controller');
+
+router.get('/', gamesControlller.index);
+router.get('/game/:id', gamesControlller.game);
+
+module.exports = router;
